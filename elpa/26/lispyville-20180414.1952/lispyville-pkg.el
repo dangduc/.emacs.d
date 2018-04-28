27e440c9ea1408bc8ac09d3401e51bc11ca4883c
Duc Dang <me@dangduc.com>
2018-04-28 13:36:13 -0700

    vendoring elpa

---
+++ elpa/26/lispyville-20180414.1952/lispyville-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lispyville" "20180414.1952" "A minor mode for integrating evil with lispy." '((lispy "0") (evil "1.2.12") (cl-lib "0.5") (emacs "24.4")) :commit "d2491462647fc6cffcc8f40c2a518a4948f2afcb" :keywords '("vim" "evil" "lispy" "lisp" "parentheses") :url "https://github.com/noctuid/lispyville")
