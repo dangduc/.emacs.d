6131bdd963c4d73db78b127f12aa6606bf405b41
Duc Dang <me@dangduc.com>
2020-09-11 00:22:13 -0700

    Move org-babel config to the bottom custom-set-variables declaration
    Having two custom-set-variable declarations confuses emacs and causes
    the auto-generated custom-set-variables block to get moved into the
    custom-set-variable block for org-babel.

--- init.el
+++ init.el
@@ -102,12 +102,6 @@
 (setq org-descriptive-links nil)
 
 ;; org-babel
-(custom-set-variables
- '(org-babel-load-languages (quote ((emacs-lisp . t)
-                                    (R . t)
-                                    (ditaa . t)
-                                    (plantuml . t))))
- '(org-confirm-babel-evaluate nil))
 (setq org-ditaa-jar-path "~/.emacs.d/vendor/not-elisp/ditaa0_9.jar")
 (setq org-plantuml-jar-path "~/.emacs.d/vendor/not-elisp/plantuml.jar")
 
@@ -1089,6 +1083,14 @@ _p_: project  ^ ^                 _c_: customize
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
+ ;; org-babel configuration edited by hand.
+ '(org-babel-load-languages (quote ((emacs-lisp . t)
+                                    (R . t)
+                                    (ditaa . t)
+                                    (plantuml . t))))
+ '(org-confirm-babel-evaluate nil)
+
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
